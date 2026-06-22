25a43ecc55506691a7278094029f416d28eb586f
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2026-06-22 20:46:28 -0300

    fix: Integração frontende e API

--- api/main.py
+++ api/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 
 app = FastAPI()
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"]
+)
+
+# Router
 app.include_router(router)
 
 
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,4 +1,4 @@
-
+const API_URL = "http://localhost:8000"
 
 document.getElementById("form-post-tree")
     .addEventListener("submit", (event) => {
@@ -8,8 +8,24 @@ document.getElementById("form-post-tree")
         send_form(formData);
     });
 
-function send_form(formData) {
-
+async function send_form(formData) {
     const formValues = Object.fromEntries(formData);
-    
+
+    const payload = {
+        "temperature": formValues.temperature,
+        "humidity": formValues.humidity,
+        "is_raining": formValues.is_raining == "on",
+    }
+    try {
+        response = await fetch(`${API_URL}/tree`, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(await response.json());
+    } catch (error) {
+        console.log(error);
+    }
 }

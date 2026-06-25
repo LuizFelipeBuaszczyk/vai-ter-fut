1f7408de64c06d20a5c2dd4e36a0086cf36e95a1
Leonardo <leopfollador@gmail.com>
2026-06-25 16:25:27 +0000

    Revert: Logica antiga mantida

--- api/train.py
+++ api/train.py
@@ -1,36 +1,25 @@
 import pandas as pd
 from sklearn import tree
 
-# Carrega o dataset
-df = pd.read_csv('weatherHistory.csv')
+# Dataset
+dados_entrada = {
+    'temperatura': [30, 25, 16, 23, 35, -3, 10, 28, 22, 18, 5, 32, 26, 12, -1, 29, 21, 37, 15, 24,11, 34, 8, 20, 19, 27, 36, 14, 17, 31, 33, 9, 3, 38, 25, 22, 16, 28, 30, 13],
+    'esta_chovendo': [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1,0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    'percentual_umidade': [45, 85, 50, 90, 40, 15, 60, 55, 95, 62, 70, 35, 80, 58, 85, 50, 65, 88, 45, 75,55, 40, 65, 85, 50, 60, 45, 70, 55, 90, 40, 75, 80, 50, 45, 95, 60, 50, 40, 65],
+    'pode_jogar': [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0,0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0]
+}
 
-# Pega a coluna original e cria a coluna temperatura
-df['temperatura'] = df['Temperature (C)']
+df = pd.DataFrame(dados_entrada)
 
-df['percentual_umidade'] = df['Humidity'] * 100
+X = df.iloc[:,:3]
+Y = df.iloc[:,-1] 
 
-df['esta_chovendo'] = df['Precip Type'].apply(lambda x: 1 if x == 'rain' else 0)
+## Treina
+clf = tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(criterion='gini')
+clf = clf.fit(X,Y)
 
+## Predição - temperatura, esta_chovendo, percentual_umidade
+result = clf.predict([[10, 0, 50]])
 
-# A regra de se pode jogar ou não
-def regra_do_jogo(linha):
-    condicao_temperatura = 15 <= linha['temperatura'] <= 32
-    condicao_umidade = linha['percentual_umidade'] < 90
-    sem_chuva = linha['esta_chovendo'] == 0
-    
-    if condicao_temperatura and condicao_umidade and sem_chuva:
-        return 1 # Vai ter fut!
-    else:
-        return 0 # Sem fut :(
-
-# aplica a regra pra todas linhas do arquivo
-df['pode_jogar'] = df.apply(regra_do_jogo, axis=1)
-
-
-# Separa os dados x,y
-X = df[['temperatura', 'esta_chovendo', 'percentual_umidade']]
-Y = df['pode_jogar']
-
-# Treina a arvore de decisao, limitado a 5 para nao ficar muito complexa
-clf = tree.DecisionTreeClassifier(criterion='gini', max_depth=5)
-clf = clf.fit(X, Y)
+print(result)
